c85544a8bd7543f4c0c4669e4bc865bebbf170a6
Dariusz Gafka <dgafka.mail@gmail.com>
2021-02-01 21:09:25 +0100

    lesson 2

---
+++ src/Domain/Product/ProductNotifier.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Domain\Product;
+
+use Ecotone\Modelling\Annotation\EventHandler;
+
+class ProductNotifier
+{
+    /**
+     * @EventHandler()
+     */
+    public function notifyAbout(ProductWasRegisteredEvent $event) : void
+    {
+        echo "Product with id {$event->getProductId()} was registered!\n";
+    }
+}
---
+++ src/Domain/Product/ProductService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Domain\Product;
+
+use Ecotone\Modelling\Annotation\CommandHandler;
+use Ecotone\Modelling\Annotation\QueryHandler;
+use Ecotone\Modelling\EventBus;
+
+class ProductService
+{
+    private array $registeredProducts = [];
+
+    /**
+     * @CommandHandler()
+     */
+    public function register(RegisterProductCommand $command, EventBus $eventBus) : void
+    {
+        $this->registeredProducts[$command->getProductId()] = $command->getCost();
+
+        $eventBus->send(new ProductWasRegisteredEvent($command->getProductId()));
+    }
+
+    /**
+     * @QueryHandler()
+     */
+    public function getPrice(GetProductPriceQuery $query) : int
+    {
+        return $this->registeredProducts[$query->getProductId()];
+    }
+}
